1d812e37a4e0f529ef17f4a315e9e311dbcc031e
Starkissed684 <aceellis684@gmail.com>
2025-09-24 09:35:18 -0700

    Title screen buttons done and also colison working more

--- Assets/What Ive made/Scripts/darkgreenmove.cs
+++ Assets/What Ive made/Scripts/darkgreenmove.cs
@@ -17,7 +17,7 @@ public class darkgreenmove : MonoBehaviour
     {
         if (collision.gameObject.tag == "wall")
         {
-            darkgreen.linearVelocity = darkgreen.linearVelocity * new Vector2(-1, 1);
+            darkgreen.linearVelocity = new Vector2(-1,1);
             Debug.Log("touched wall");
         }
     }
